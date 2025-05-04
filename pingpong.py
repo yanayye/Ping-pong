9f06ee149efe72ece37d859d7fbf4f2c2170cbe8
yanayye <78418616+yanayye@users.noreply.github.com>
2025-05-04 14:31:29 +0300

    Update pingpong.py
    Добавлены ракетки

--- pingpong.py
+++ pingpong.py
@@ -1,5 +1,33 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+   #конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+        super().__init__()
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self): #перемещение для левой ракетки
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 200:
+            self.rect.y += self.speed
+    def update_r(self): #перемещение для правой
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 200:
+            self.rect.y += self.speed
+            
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
@@ -12,6 +40,9 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+# картинка, координата х, координата y, скорость, размер по х, размер по y
+player_r = Player("racket.png", 620, 200, 5, 50, 200)
+player_l = Player("racket.png",30, 200 , 5, 50, 200)
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -19,6 +50,11 @@ while game:
     if finish != True:
         # window.blit(background,(0, 0))
         window.fill(BACK)
+        player_l.reset()
+        player_r.reset()
+
+        player_r.update_r()
+        player_l.update_l()
 
     display.update()
     clock.tick(FPS)
